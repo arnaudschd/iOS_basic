d6e3758d93c44e3d2f1d7d6a6746632230c397d0
Arnaud SCHEID <arnaud.scheid@epitech.eu>
2020-11-23 13:49:06 +0100

    Project creation

---
+++ iOS_basics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_basics
+//
+//  Created by Arnaud SCHEID on 23/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

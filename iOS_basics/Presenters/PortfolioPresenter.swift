94a219e2e0c96bee04ef54553495a1f4fd1a8f6f
Arnaud SCHEID <arnaud.scheid@epitech.eu>
2020-11-30 13:57:23 +0100

    Trad + MVP wip

--- iOS_basics/Presenters/PortfolioPresenter.swift
+++ iOS_basics/Presenters/PortfolioPresenter.swift
@@ -7,6 +7,37 @@
 
 import Foundation
 
-class PortfolioPresenter {
+final class PortfolioPresenter {
+      
+    private var currencySorted = false
+    private var priceSorted = false
+    private var ownedSorted = false
     
+    func getMarketDatas() {
+        if let data = MockHelpers.readLocalFile(forName: "CurrenciesMocks") {
+            AppManager.investmentManager.currencies = try! JSONDecoder().decode([Currency].self, from: data)
+            for item in AppManager.investmentManager.currencies {
+                AppManager.userManager.user.ownedCurrencies[item.assetID] = 0.0
+            }
+            AppManager.userManager.user.ownedCurrencies["USDT"] = 100000
+        }
+    }
+    
+    func sortCurrency() {
+        if currencySorted == true {
+            AppManager.investmentManager.currencies.sort { $0.assetID < $1.assetID }
+        } else {
+            AppManager.investmentManager.currencies.sort { $0.assetID > $1.assetID }
+        }
+        currencySorted = !currencySorted
+    }
+    
+    func sortPrice() {
+        if priceSorted == true {
+            AppManager.investmentManager.currencies.sort { $0.price < $1.price }
+        } else {
+            AppManager.investmentManager.currencies.sort { $0.price > $1.price }
+        }
+        priceSorted = !priceSorted
+    }
 }

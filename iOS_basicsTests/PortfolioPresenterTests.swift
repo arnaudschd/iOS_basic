99ed2f6a3fa55e82819ebdbaedee54e0596d9e67
Arnaud SCHEID <arnaud.scheid@epitech.eu>
2021-01-06 14:05:27 +0100

    portfolio tests

--- iOS_basicsTests/PortfolioPresenterTests.swift
+++ iOS_basicsTests/PortfolioPresenterTests.swift
@@ -5,4 +5,22 @@
 //  Created by Arnaud SCHEID on 06/01/2021.
 //
 
-import Foundation
+import XCTest
+
+@testable import iOS_basics
+
+final class PortfolioPresenterTests: XCTestCase {
+    let investPres = InvestmentPresenter()
+    let sut = PortfolioPresenter()
+    
+    override func setUp() {
+        investPres.getMarketDatas()
+    }
+
+    
+    func testGetOwnedCurrencies_success() {
+        let res = sut.getOwnedCurrencies()
+        
+        XCTAssertEqual(res, 2)
+    }
+}
